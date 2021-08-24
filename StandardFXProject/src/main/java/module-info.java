2414330c290782f5df4251c3f1a3f0b97e11c8df
Bernhard Wallisch <bernhard_wallisch@hotmail.com>
2021-08-24 19:54:06 +0200

    removed StandardFXProject

--- StandardFXProject/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module at.fhtw.disys.standardfxproject.standardfxproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens at.fhtw.disys.standardfxproject.standardfxproject to javafx.fxml;
-    exports at.fhtw.disys.standardfxproject.standardfxproject;
-}
