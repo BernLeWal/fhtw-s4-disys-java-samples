b7dfb5f16c9de97d2e2939899fc4088cf0f68f7f
Bernhard Wallisch <bernhard_wallisch@hotmail.com>
2021-08-26 15:20:26 +0200

    Updates along to the documentation

--- HelloWorld/src/main/java/at/fhtw/disys/helloworld/HelloApplication.java
+++ HelloWorld/src/main/java/at/fhtw/disys/helloworld/HelloApplication.java
@@ -10,8 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(
-                HelloApplication.class.getResource("hello.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader( HelloApplication.class.getResource("hello.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello World!");
         stage.setScene(scene);
