5c9d3dcc2b73b825aada5491bba25b474d6b2105
Bernhard Wallisch <bernhard_wallisch@hotmail.com>
2021-08-26 15:19:27 +0200

    Updates along with documentation

--- FirstFXProject/src/main/java/at/fhtw/disys/firstfxproject/firstfxproject/HelloApplication.java
+++ FirstFXProject/src/main/java/at/fhtw/disys/firstfxproject/firstfxproject/HelloApplication.java
@@ -2,6 +2,7 @@ package at.fhtw.disys.firstfxproject.firstfxproject;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,7 +12,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Parent parent = fxmlLoader.load();
+        Scene scene = new Scene(parent, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
