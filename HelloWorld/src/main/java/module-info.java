8c9b4e2d81865397252afffeb5b0219f1cdcfbdc
Bernhard Wallisch <bernhard_wallisch@hotmail.com>
2021-10-17 12:02:33 +0200

    renamed HelloWorld to HelloJavaFX

--- HelloWorld/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module at.fhtw.disys.helloworld {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens at.fhtw.disys.helloworld to javafx.fxml;
-    exports at.fhtw.disys.helloworld;
-}
